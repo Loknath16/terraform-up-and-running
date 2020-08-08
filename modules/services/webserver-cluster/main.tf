643cc27338f5837f441f08a7f1134423a685d714
Loknath Chadalavada <loknath16@gmail.com>
2020-08-08 18:44:16 -0400

    Initial commit of modules repo

---
+++ modules/services/webserver-cluster/main.tf
@@ -0,0 +1,172 @@
+
+# ASG Launch Configuration
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data       = data.template_file.user_data.rendered
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# data source for VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# data source for  Subnet IDs (AZs)
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# terraform remote state datastore
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = var.remote_state_bucket
+    key    = var.db_remote_state_key
+    region = "us-east-2"
+  }
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/user-data.sh")
+
+  vars = {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }
+}
+
+
+# ASG Configuration
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  # ALB configuration
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+# ALB Configuration
+resource "aws_lb" "example" {
+  name               = "${var.cluster_name}-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+  
+}
+
+# AWS LB Listener Config
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = local.http_port
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+#  ALB Listener Rule (acts as a bridge between listener config and target group)
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+# ALB Target Group
+resource "aws_lb_target_group" "asg" {
+  name     = "${var.cluster_name}-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# SG for ALB to allow port 80 (incoming)
+resource "aws_security_group" "alb" {
+  name = "${var.cluster_name}-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
+  }
+}
+
+
+
+# Security Group for EC2
+resource "aws_security_group" "instance" {
+  name = "${var.cluster_name}-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = tcp_protocol
+    cidr_blocks = local.all_ips
+  }
+}
+
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+  health_check_type = "ELB"
+}
+
+
+
+
+
